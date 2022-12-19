63ac2719fb08fbf63da5b0fcdafd3a8ca3c560c3
Sree Tej Peddaiahgari <sreetejpeddaiahgari@gmail.com>
2022-12-18 23:30:43 -0600

    added swift project

---
+++ SwiftAlgorithms/SwiftAlgorithms/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftAlgorithms
+//
+//  Created by Sree Tej Peddaiahgari on 12/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
